018c61ad9c50d2e91a43f4596fe52bb67a6d918f
Shashank <shashushashankk@gmail.com>
2025-04-08 23:13:07 +0530

    Commited new changes

--- src/test/java/Login/LoginToZoodAppTest.java
+++ src/test/java/Login/LoginToZoodAppTest.java
@@ -4,6 +4,11 @@ import com.zoodel.StepGroup.StepGroup;
 import com.zoodel.generic_android.BaseClass;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
 public class LoginToZoodAppTest extends BaseClass {
     @Test
     public void userAbleToLoginToZoodTest() {
@@ -17,4 +22,25 @@ public class LoginToZoodAppTest extends BaseClass {
 
     }
 
+    public static void main() throws InterruptedException {
+
+        try {
+            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "appium -a localhost -p 4723 --base-path wd/hub");
+            pb.redirectErrorStream(true);
+            Process process=pb.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line= null;
+            while ((line=reader.readLine())!=null){
+                System.out.println(line);
+                if(line.contains("Appium REST http interface listener started on")){
+                    break;
+                }
+            }
+
+            System.out.println("Appium server started...");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
